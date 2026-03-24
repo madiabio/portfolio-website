7a808e5b0c7acb4b3882982e329ec8de7195fc3d
madiabio <madelineabio@gmail.com>
2026-03-24 20:09:31 +1000

    fix(api): fix import paths

--- apps/api/src/analytics/analytics.module.ts
+++ apps/api/src/analytics/analytics.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AnalyticsService } from './analytics.service';
 import { AnalyticsController } from './analytics.controller';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
